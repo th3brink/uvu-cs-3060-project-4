6f0cf6fc59f50823a0a0ae2be540f71783448efb
Kory Kehl <korykehl@mac.com>
2013-03-07 13:22:21 -0700

    Added code to print lines as well as reverse video with percentage

--- more1.c
+++ more1.c
@@ -1,10 +1,56 @@
+// Student: Kory Kehl, James Brinkerhoff, Bryson Murray
+// Instructor: John Jolly
+// Class: CS 3060-001
+// Project: 4
+// Date: Feb 26, 2013
+// Description: Simulates the more command
+
 /* stdio for printf, fprintf, perror, fopen, feof and fgets */
 #include<stdio.h>
 /* stdlib for exit */
 #include<stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
 /* Declare my buffer size constant */
 #define BUFFER_SIZE (100)
+#define O_RDONLY (100)
+// Number of lines to print
+#define PAGELEN (23)
+// Length of each line to print
+#define LINELEN (512)
 
+// do_more function
+// Description: Prints 23 lines to display at a time or 1 the calls see_more
+//              for a reply on how many more lines to print
+// Return: None
+// Paramaters: Takes File *
+void do_more(FILE *);
+
+// see_more function
+// Description: Prints in reverse video percentage of file displayed
+// Return: int
+// Paramaters: Takes File *, char *
+int see_more(FILE *, char*);
+
+// find_num_of_lines function
+// Description: Finds the number of lines in file
+// Return: None
+// Paramaters: Takes File *
+void find_num_of_lines(FILE *);
+
+// deleteSpace function
+// Description: Deletes what was printed in reverse video
+// Return: None
+// Paramaters: Takes int
+void deleteSpace(int);
+
+// Total number of lines
+int numLines;
+// Total number of lines displayed
+int linesDisplayed;
+// File name
+char *fileName;
 
 //function for handling signals
 //*** not sure what this need to take in and even if this function needs to be split into different function for each handler.
@@ -48,32 +94,149 @@ void printLines (int lines, int fileExists) {
  
 
 //main function
-int main (int argc, char * argv[]) {
-
-	FILE * fp;
-	char buffer[BUFFER_SIZE];
-
-	if(argc <= 1) {
-        	//fprintf(stderr, "No file option provided\n");
-		// set stdin to stdout
-	     //if no file then copy stdin to stdout (it allows for pipes and redirections)
-		  
-		//int fd_tty;
-		//FILE *fp_tty;
-
-		//fd_tty = open("/dev/tty", O_RDONLY);
-
-		//fp_tty = fdopen(fd_tty, "r");
-
-                
-        } else if (argc > 1) {
-		//if file then copy the contents of the named file to stdout (only one argument/file is expected)
-	}
-
-     //program should use tcgetattrs to save the terminal attributes when it first starts. Before terminating it should use tcsetattrs to
-	// restore the original terminalattributes.
-	return 0;
+int main (int argc, char * argv[])
+{
+    FILE *fp;
+    //int fd_tty;
+    //FILE *fp_tty;
+    //fd_tty = open("/dev/tty", O_RDONLY);
+    //fp_tty = fdopen(fd_tty, "r");
+    
+    
+    // More then 1 argument, so quit
+    if(argc > 2)
+    {
+        exit(1);
+    }
+    // No file name passed in
+    if(argc == 1)
+    {
+        do_more(stdin);
+    }
+    // One file name is passed in
+    else
+    {
+        if((fp = fopen(argv[1], "r")) != NULL)
+        {
+            linesDisplayed = 0;
+            find_num_of_lines(fp);
+            fileName = argv[1];
+            do_more(fp);
+            fclose(fp);
+        }
+    }
+    return 0;
 }
+
+// Prints each increment of lines
+void do_more(FILE *fp)
+{
+    // Stores each line
+    char line[LINELEN];
+    // Store infor to print in reverse video
+    char toPrint[50];
+    // Increment of lines printed
+    int num_of_lines = 0;
+    // Percent of page printed
+    int pagePercent;
+    int see_more(FILE *, char*), reply;
+    FILE *fp_tty;
+    
+    fp_tty = fopen("/dev/tty", "r");
+    if(fp_tty == NULL)
+    {
+        exit(1);
+    }
+    
+    while(fgets(line, LINELEN, fp))
+    {
+        if(num_of_lines == PAGELEN)
+        {
+            pagePercent = (linesDisplayed * 100) / numLines;
+            if(linesDisplayed == PAGELEN)
+            {
+                // Stores what to print in reverse video
+                // File Name and Percentage
+                sprintf(toPrint, "%s %%%d", fileName, pagePercent);
+                reply = see_more(fp_tty, toPrint);
+            }
+            if(linesDisplayed > PAGELEN)
+            {
+                // Stores what to print in reverse video
+                // Percentage
+                sprintf(toPrint, "%%%d", pagePercent);
+                reply = see_more(fp_tty, toPrint);
+            }
+            if(reply == 0)
+            {
+                break;
+            }
+            num_of_lines -= reply;
+        }
+        if(fputs(line, stdout) == EOF)
+        {
+            exit(1);
+        }
+        num_of_lines++;
+        linesDisplayed++;
+    }
+}
+
+// Prints percentage and waits for command to print more
+int see_more(FILE *cmd, char *buf)
+{
+    int c;
+    
+    // Prints revers video
+    printf("\033[7m %s \033[m", buf);
+    while((c = getc(cmd)) != EOF)
+    {
+        if(c == 'q')
+        {
+            // Call to delete what printed in reverse video
+            deleteSpace((int)strlen(buf) + 2);
+            return 0;
+        }
+        if(c == ' ')
+        {
+            // Call to delete what printed in reverse video
+            deleteSpace((int)strlen(buf) + 2);
+            return PAGELEN;
+        }
+        if(c == 'n')
+        {
+            // Call to delete what printed in reverse video
+            deleteSpace((int)strlen(buf) + 2);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Finds how many lines the document has
+void find_num_of_lines(FILE *fp)
+{
+    char line[LINELEN];
+    int lines = 0;
+    while(fgets(line, LINELEN, fp))
+    {
+        lines++;
+    }
+    numLines = lines;
+    clearerr(fp);
+    fseek(fp, 0, SEEK_SET);
+}
+
+// Delete characters from reverse video print with percentage
+void deleteSpace(int n)
+{
+    int i = 0;
+    for(i = 0; i < n; i++)
+    {
+        printf("\b \b");
+    }
+}
+
 
 //Notes
 	//To erase a character from the screen, display a backspace character (\b), a blank, and another backspace character.
